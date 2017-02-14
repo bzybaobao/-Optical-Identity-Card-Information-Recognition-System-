405575e0bf79d5d8ec41ccaaae8591094d54de4a
Zhiwei Bao <1242428997@qq.com>
2017-02-14 12:35:29 -0600

    Create test.java

---
+++ bzy/test.java
@@ -0,0 +1,33 @@
+
+public class data {
+	private int score;
+	private int level;
+	private double speed;
+	private int pixelSize;
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+	public int getPixelSize() {
+		return pixelSize;
+	}
+	public void setPixelSize(int pixelSize) {
+		this.pixelSize = pixelSize;
+	}
+
+
+}
